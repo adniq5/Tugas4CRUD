5160d83ef6ea1332c350b0c72b0dae7ab1b90d41
adniq5 <adniq55@gmail.com>
2024-12-27 20:51:48 +0800

    U:Perubahan Struktur Data dan Menu Dasar

--- Tugas4CRUD.cpp
+++ Tugas4CRUD.cpp
@@ -1,63 +1,55 @@
 #include <iostream>
-#include <conio.h>
+#include <cstdlib>  // Untuk system("cls")
+#include <conio.h>  // Untuk getch()
 using namespace std;
 
+// Struktur Data Mahasiswa
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    string tanggalLahir; // Format DD/MM/YYYY
+    float ipk;
+};
 
-void dMenu(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
-
+// Fungsi untuk menampilkan menu
+void tampilkanMenu() {
+    system("cls");
+    cout << "\n=== Menu Aplikasi Data Mahasiswa ===\n";
+    cout << "1. Masukkan Data Mahasiswa\n";
+    cout << "2. Tampilkan Data Mahasiswa\n";
+    cout << "3. Update Data Mahasiswa\n";
+    cout << "4. Hapus Data Mahasiswa\n";
+    cout << "5. Exit\n";
+    cout << "Masukkan pilihan: ";
 }
-
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
-}
-
 
 int main() {
-char pl;
-do
-{
-    dMenu();
-    pl=getch();
-  switch (pl)
-  {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    mPertama("ke- empat");
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
-  }
-
-
-} while (pl!='5');
-  return 0;
+    char pilihan;
+    do {
+        tampilkanMenu();
+        pilihan = getch(); // Mengambil input dari pengguna
+        // Menangani pilihan menu
+        switch (pilihan) {
+            case '1':
+                // Tambah data
+                break;
+            case '2':
+                // Tampilkan data
+                break;
+            case '3':
+                // Update data
+                break;
+            case '4':
+                // Hapus data
+                break;
+            case '5':
+                cout << "\nTerima kasih telah menggunakan program ini.\n";
+                break;
+            default:
+                cout << "\nPilihan tidak valid. Coba lagi.\n";
+                break;
+        }
+    } while (pilihan != '5');
+    return 0;
 }
